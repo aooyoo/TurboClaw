a2369347f839bbce29b3dc5de73962da588210b4
aooyoo <qiaohk@gmail.com>
2026-02-26 12:07:58 +0800

    feat: Integrate PostHog for analytics and platform information tracking.

--- TurboClaw/frontend/src/main.tsx
+++ TurboClaw/frontend/src/main.tsx
@@ -3,6 +3,25 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { I18nProvider } from './i18n'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import { GetPlatformInfo } from '../wailsjs/go/main/App'
+
+posthog.init('phc_MblKGOIAowjXQWwxTMII8Ng2eu0iHm6uzloGI4Ai0sp', {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'always', // Always capture profiles
+    capture_pageview: true,
+    enable_recording_console_log: true,
+})
+
+// Fetch platform info and register as super properties
+GetPlatformInfo().then((info) => {
+    posthog.register({
+        os: info.os,
+        arch: info.arch,
+        client_type: 'desktop_app',
+    })
+}).catch(console.error)
 
 const container = document.getElementById('root')
 
@@ -10,8 +29,10 @@ const root = createRoot(container!)
 
 root.render(
     <React.StrictMode>
-        <I18nProvider>
-            <App />
-        </I18nProvider>
+        <PostHogProvider client={posthog}>
+            <I18nProvider>
+                <App />
+            </I18nProvider>
+        </PostHogProvider>
     </React.StrictMode>
 )
